38db11735599f246dc607479e80b5a36ddecfd39
sebneedles <sebneedles@gmail.com>
2023-02-08 04:45:24 +0100

    3 - Design Navbar & Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,58 @@
 import React from 'react';
+import './Footer.scss';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            quasi, libero ratione fugiat exercitationem sapiente laboriosam
+            molestias accusantium voluptatibus qui. A nisi itaque autem, ab
+            excepturi minima nihil quos corrupti!
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A, rem sunt
+            illum expedita quasi magnam ipsa harum iste nam officia aliquid.
+            Quibusdam repellendus provident dicta deleniti quisquam earum vel
+            ratione!
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">MyStore</span>
+          <span className="copyright">
+            © Copyright 2023. All Rights Reserved
+          </span>
+        </div>
+        <div className="right">
+          <img src="./img/payment.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,78 @@
 import React from 'react';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { Link } from 'react-router-dom';
+import './Navbar.scss';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img src="/img/en.png" alt="US/UK" />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/1">
+              Women
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/2">
+              Men
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/3">
+              Children
+            </Link>
+          </div>
+        </div>
+        <div className="center">
+          <Link to="/">MyStore</Link>
+        </div>
+        <div className="right">
+          <div className="item">
+            <Link className="link" to="/">
+              Homepage
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              About
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Contact
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Stores
+            </Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonOutlineIcon />
+            <FavoriteBorderIcon />
+            <div className="cartIcon">
+              <AddShoppingCartIcon />
+              <span>0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
