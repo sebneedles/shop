1892e50fcd350bbbc10a0451768a015b126bfa1d
sebneedles <sebneedles@gmail.com>
2023-02-09 11:00:48 +0100

    6 - homepage grid systems

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -6,8 +6,8 @@ const FeaturedProducts = ({ type }) => {
   const data = [
     {
       id: 1,
-      img: 'https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress@cs=tinysrgb@w=1600',
-      img2: 'https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress@cs=tinysrgb@w=1600',
+      img: 'https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress@cs=tinysrgb&w=400',
+      img2: 'https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress@cs=tinysrgb&w=400',
       title: 'Long Sleeve Graphic T-Shirt',
       isNew: true,
       oldPrice: 19,
@@ -15,7 +15,7 @@ const FeaturedProducts = ({ type }) => {
     },
     {
       id: 2,
-      img: 'https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress@cs=tinysrgb@w=1600',
+      img: 'https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress@cs=tinysrgb&w=400',
       title: 'Coat',
       isNew: true,
       oldPrice: 19,
@@ -23,14 +23,14 @@ const FeaturedProducts = ({ type }) => {
     },
     {
       id: 3,
-      img: 'https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress@cs=tinysrgb@w=1600',
+      img: 'https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress@cs=tinysrgb&w=400',
       title: 'Skirt',
       oldPrice: 19,
       price: 12,
     },
     {
       id: 4,
-      img: 'https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress@cs=tinysrgb@w=1600',
+      img: 'https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress@cs=tinysrgb&w=400',
       title: 'Hat',
       oldPrice: 19,
       price: 12,
