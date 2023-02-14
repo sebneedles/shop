653c2447c902aa1e7508c3d9887541f81e507a7d
sebneedles <sebneedles@gmail.com>
2023-02-14 20:09:11 +0100

    14 - Redux configuration Cart / Store

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,48 +1,50 @@
 import React from 'react';
 import './Cart.scss';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeItem, resetCart } from '../../redux/cartReducer';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
-  const data = [
-    {
-      id: 1,
-      img: 'https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress@cs=tinysrgb&w=200',
-      title: 'Long Sleeve Graphic T-Shirt',
-      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, similique?',
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 2,
-      img: 'https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress@cs=tinysrgb&w=200',
-      title: 'Coat',
-      desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, similique?',
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-  ];
+  const products = useSelector((state) => state.cart.products);
+  const dispatch = useDispatch();
+  const totalPrice = () => {
+    let total = 0;
+    products.forEach((item) => (total += item.quantity * item.price));
+    return total.toFixed(2);
+  };
   return (
     <div className="cart">
       <h1>Product in your cart</h1>
-      {data?.map((item) => (
+      {products?.map((item) => (
         <div className="item" key={item.id}>
-          <img src={item.img} alt="item img" />
+          <Link to={`/product/${item.id}`}>
+            <img
+              src={process.env.REACT_APP_UPLOAD_URL + item.img}
+              alt="item img"
+            />
+          </Link>
           <div className="details">
             <h1>{item.title}</h1>
             <p>{item.desc?.substring(0, 100)}</p>
-            <div className="price">1 x ${item.price}</div>
+            <div className="price">
+              {item.quantity} x ${item.price}
+            </div>
           </div>
-          <DeleteOutlinedIcon className="delete" />
+          <DeleteOutlinedIcon
+            className="delete"
+            onClick={() => dispatch(removeItem(item.id))}
+          />
         </div>
       ))}
       <div className="total">
         <span>SUBTOTAL</span>
-        <span>$123</span>
+        <span>${totalPrice()}</span>
       </div>
       <button>PROCEED TO CHECKOUT</button>
-      <span className="reset">Reset Cart</span>
+      <span className="reset" onClick={() => dispatch(resetCart())}>
+        Reset Cart
+      </span>
     </div>
   );
 };
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -7,9 +7,11 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
 import Cart from '../Cart/Cart';
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const products = useSelector((state) => state.cart.products);
 
   return (
     <div className="navbar">
@@ -74,7 +76,7 @@ const Navbar = () => {
             <FavoriteBorderIcon />
             <div className="cartIcon" onClick={() => setOpen(!open)}>
               <AddShoppingCartIcon />
-              <span>0</span>
+              <span>{products.length}</span>
             </div>
           </div>
         </div>
